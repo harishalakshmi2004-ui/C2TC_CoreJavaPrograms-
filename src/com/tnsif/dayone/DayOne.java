85f4179f513afe6403e9c9949f0df8ce0e883872
haris <haris@LAPTOP-CM20LHP3>
2025-12-15 23:17:31 +0530

    Initial commit

---
+++ src/com/tnsif/dayone/DayOne.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayone;
+
+public class DayOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HII HARISHA");
+
+	}
+
+}
