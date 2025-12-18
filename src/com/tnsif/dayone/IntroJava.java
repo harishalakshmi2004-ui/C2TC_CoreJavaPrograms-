7a0be60c6f1037def65e47f703c871d876204aab
haris <haris@LAPTOP-CM20LHP3>
2025-12-18 21:39:35 +0530

    add java source files

---
+++ src/com/tnsif/dayone/IntroJava.java
@@ -0,0 +1,30 @@
+package com.tnsif.dayone;
+
+public class IntroJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubpackage com.tnsif.introofjava;
+        int age = 21;
+		double salary = 50000.00;
+		boolean isPassed = true;
+		char grade = 'A';
+		
+		
+		double newSalary = age; 
+		int roundSalary = (int)salary; 
+		int increment = 5000;
+		
+		int total = roundSalary + increment;
+		
+		
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(roundSalary);
+		System.out.println(total);
+
+	}
+
+}
+
+
+	
