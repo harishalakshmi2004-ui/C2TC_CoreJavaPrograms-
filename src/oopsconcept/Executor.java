1f320dde1f34db707beeb7bc8018ba4cff6ec8cd
haris <haris@LAPTOP-CM20LHP3>
2025-12-19 00:15:28 +0530

    add source file

---
+++ src/oopsconcept/Executor.java
@@ -0,0 +1,19 @@
+package oopsconcept;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				EncapsulationDemo ED = new EncapsulationDemo();
+				ED.setName("M S Dhoni");
+				ED.setAge(44);
+				ED.setJersyNo(7);
+				
+				System.out.println(ED);
+				Executor e = new Executor();
+				System.out.println(e);
+
+			
+	  }
+	}
