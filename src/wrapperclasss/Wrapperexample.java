776c11af0307268f16d831ffc8fe73de12a717f2
Harisha R <https://github.com/harishalakshmi2004-ui/C2TC_CoreJavaPrograms->
2025-12-28 21:59:40 +0530

    WRAPPERCLASS

---
+++ src/wrapperclasss/Wrapperexample.java
@@ -0,0 +1,28 @@
+package wrapperclasss;
+
+public class Wrapperexample {
+
+	public static void main(String[] args) {
+		
+				//primitive into object --> autoboxing 
+
+				
+				int x = 125;
+				
+				Integer y = Integer.valueOf(x);
+				
+				
+				System.out.println(y);
+				
+				
+				// object into primitive --> unboxing
+				
+				int z = y;
+				
+				System.out.println(z);
+
+					}
+
+	}
+
+
