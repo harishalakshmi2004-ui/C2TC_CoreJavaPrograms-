499ee9826ea5700bde0b7cc25cc76d649e9bb9fd
Harisha R <https://github.com/harishalakshmi2004-ui/C2TC_CoreJavaPrograms->
2025-12-28 22:49:15 +0530

    custom

---
+++ src/exceptionhandling2/Finalblock.java
@@ -0,0 +1,28 @@
+package exceptionhandling2;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Finalblock {
+
+	
+		
+		public static void main(String[] args) {
+				Scanner scanner = new Scanner(System.in);
+				try {
+					System.out.print("Enter the number : ");
+					int number = scanner.nextInt();
+					System.out.println((number % 2 == 0 )? " is Even" : " is Odd");
+					return;
+				} catch (InputMismatchException e) {
+					System.err.println("Invalid input...");
+
+				} finally {
+					System.out.println("In Finally....");
+					scanner.close(); 
+				}
+			}
+		
+	}
+
+
